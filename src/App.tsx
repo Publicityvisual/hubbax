15a63671fddaf349d6321e9d0e1650331f982a8a
FlixCentral Development <dev@flixcentralplus.com>
2026-04-07 21:27:29 -0600

    Fix: Add FirebaseProvider to App root

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { FirebaseProvider } from './contexts/FirebaseContext';
 import LoginPage from './pages/auth/LoginPage';
 import { SupremeAdminLogin } from './pages/auth/SupremeAdminLogin';
 import HomePage from './pages/home/HomePage';
@@ -17,28 +18,30 @@ import RecoveryPage from './pages/auth/RecoveryPage';
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <VersionManager />
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          
-          <Route path="/feed" element={<HomePage />} />
-          <Route path="/profile/:username" element={<ProfilePage />} />
-          <Route path="/me" element={<ProfilePage />} />
-          
-          {/* New Pages */}
-          <Route path="/friends" element={<FriendsPage />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          
-          {/* Auth Pages */}
-          <Route path="/supreme-admin" element={<SupremeAdminLogin onSupremeLogin={() => console.log('Supreme admin login attempted')} />} />
-          <Route path="/recovery" element={<RecoveryPage />} />
-          <Route path="/legal/*" element={<LoginPage />} />
-          <Route path="/help/*" element={<LoginPage />} />
-        </Routes>
-      </Router>
+      <FirebaseProvider>
+        <Router>
+          <VersionManager />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            
+            <Route path="/feed" element={<HomePage />} />
+            <Route path="/profile/:username" element={<ProfilePage />} />
+            <Route path="/me" element={<ProfilePage />} />
+            
+            {/* New Pages */}
+            <Route path="/friends" element={<FriendsPage />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            
+            {/* Auth Pages */}
+            <Route path="/supreme-admin" element={<SupremeAdminLogin onSupremeLogin={() => console.log('Supreme admin login attempted')} />} />
+            <Route path="/recovery" element={<RecoveryPage />} />
+            <Route path="/legal/*" element={<LoginPage />} />
+            <Route path="/help/*" element={<LoginPage />} />
+          </Routes>
+        </Router>
+      </FirebaseProvider>
     </ThemeProvider>
   );
 }
