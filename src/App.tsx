260036a0684da867980b81ff376f2f47a7244c4f
FlixCentral Development <dev@flixcentralplus.com>
2026-04-07 21:21:54 -0600

    Add ThemeContext and light mode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
 import LoginPage from './pages/auth/LoginPage';
 import { SupremeAdminLogin } from './pages/auth/SupremeAdminLogin';
 import HomePage from './pages/home/HomePage';
@@ -15,28 +16,30 @@ import RecoveryPage from './pages/auth/RecoveryPage';
 
 function App() {
   return (
-    <Router>
-      <VersionManager />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        
-        <Route path="/feed" element={<HomePage />} />
-        <Route path="/profile/:username" element={<ProfilePage />} />
-        <Route path="/me" element={<ProfilePage />} />
-        
-        {/* New Pages */}
-        <Route path="/friends" element={<FriendsPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        
-        {/* Auth Pages */}
-        <Route path="/supreme-admin" element={<SupremeAdminLogin onSupremeLogin={() => console.log('Supreme admin login attempted')} />} />
-        <Route path="/recovery" element={<RecoveryPage />} />
-        <Route path="/legal/*" element={<LoginPage />} />
-        <Route path="/help/*" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <VersionManager />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          
+          <Route path="/feed" element={<HomePage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/me" element={<ProfilePage />} />
+          
+          {/* New Pages */}
+          <Route path="/friends" element={<FriendsPage />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          
+          {/* Auth Pages */}
+          <Route path="/supreme-admin" element={<SupremeAdminLogin onSupremeLogin={() => console.log('Supreme admin login attempted')} />} />
+          <Route path="/recovery" element={<RecoveryPage />} />
+          <Route path="/legal/*" element={<LoginPage />} />
+          <Route path="/help/*" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
